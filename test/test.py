90feb87d07fa433b6f1c97007c9b52970e9c4f53
Bastien ARNOUT <barnout@e3r5p3.42.fr>
2017-11-11 10:57:26 +0100

    small improvements in test.py

--- test/test.py
+++ test/test.py
@@ -33,28 +33,34 @@ def check_solution(arg, response):
                 print("no solution file, yet...")
         return ok 
 
-if len(sys.argv) == 1:
-    exit_m("put test file as argument, you can use wildcard '*' to run a battery of tests like 'and_*' or use '*.test' to run all tests")
-args = sys.argv[1:]
-print()
-ok = 0
-for arg in args:
-    if os.path.isfile(arg):
-        h = '\033[33m#####\033[0m {:s} \033[33m#####\033[0m'.format(arg)
-        print(h)
-        #subprocess.run(["cat", arg])
-        print()
-        output = subprocess.check_output(["python3", "../Expert_System.py", arg])
-        output = output.decode("utf-8")
-        print(output, end='')
-        print('\033[33m{:s}\033[0m'.format((len(h) - 18) * '#'))
-        test = check_solution(arg, output)
-        if test == 1:
-            print("\033[32mOK\033[0m")
-            ok += 1
+def main():
+    if not os.path.isfile('../Expert_System.py'):
+        exit_m('Execute test.py in test directory')
+    if len(sys.argv) == 1:
+        exit_m("put test file as argument, you can use wildcard '*' to run a battery of tests like 'and_*' or use '*.test' to run all tests")
+    args = sys.argv[1:]
+    print()
+    ok = 0
+    for arg in args:
+        if os.path.isfile(arg):
+            h = '\033[33m#####\033[0m {:s} \033[33m#####\033[0m'.format(arg)
+            print(h)
+            with open(arg, 'r') as f:
+                print(f.read())
+            output = subprocess.check_output(["python3", '../Expert_System.py', arg])
+            output = output.decode("utf-8")
+            print(output, end='')
+            print('\033[33m{:s}\033[0m'.format((len(h) - 18) * '#'))
+            test = check_solution(arg, output)
+            if test == 1:
+                print("\033[32mOK\033[0m")
+                ok += 1
+            else:
+                print("{:s} : \033[31mKO\033[0m".format(arg))
+            print()
         else:
-            print("{:s} : \033[31mKO\033[0m".format(arg))
-        print()
-    else:
-        print("File '{:s}' not found".format(arg))
-print("\n[{:d}/{:d}] tests passed".format(ok, len(args)))
+            print("File '{:s}' not found".format(arg))
+    print("[{:d}/{:d}] tests passed".format(ok, len(args)))
+
+if __name__ == '__main__':
+    main()
